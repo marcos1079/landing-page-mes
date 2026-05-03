8c6c4c54f4157b2bd1fa60443c44693acac0066e
marcos1079 <marcos.antonio2306@outlook.com>
2026-05-03 16:14:51 -0300

    alteracoes pontuais

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen">
-      <Navbar isAdmin={false} onToggleAdmin={() => {}} />
+     
       <HeroSection />
       <ServicesSection />
       <DifferentialsSection />
